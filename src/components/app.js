2ef2056b6b62a1e8063ebe4115f6bb4a263b319a
Max <maxnelson997@gmail.com>
2018-05-30 20:15:51 -0600

    started timer

--- src/components/app.js
+++ src/components/app.js
@@ -15,6 +15,37 @@ export default class App extends Component {
     }
   }
 
+  handleGenerate = function() {
+    this.setState({ active: true })
+    // Set the date we're counting down to
+  var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    console.log(time)
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        // document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+}, 1000);
+  }.bind(this)
 
   renderItems = function() {
     if(this.state.active) {
@@ -27,7 +58,7 @@ export default class App extends Component {
     } else {
       return [
         <Picker/>,
-        Button('Generate Countdown', () => this.setState({ active: true }))
+        Button('Generate Countdown', () => this.handleGenerate())
       ]
     }
   }.bind(this)
@@ -37,7 +68,6 @@ export default class App extends Component {
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
 
-        <div className="grid__skew-dark-one-box"></div>
         <div className="grid__skew-dark-two"></div>
         <div className="grid__skew-dark-three"></div>
 
